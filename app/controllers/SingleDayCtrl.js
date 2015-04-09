2ded625e9b5cc95aeb4f042cad8c443859b5b3f5
jingpei <jingjpei@gmail.com>
2015-04-09 19:35:22 -0400

    added variable questions and answers

--- app/controllers/SingleDayCtrl.js
+++ app/controllers/SingleDayCtrl.js
@@ -3,23 +3,39 @@ function SingleDayListController( $scope, $routeParams ){
 	$scope.week_day = $routeParams.week_day;
 
 	var answers = {
+		"Monday" : [
+			{ date: '2014/10/29', entry: 'Monday entry only one'}
+		],
+		"Tuesday" : [
+			{ date: '2014/10/31', entry: 'Tuesday entry only one'}
+		],
 		"Wednesday" : [
 			{ date: '2014/10/30', entry: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in'},
 			{ date: '2014/11/07', entry: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.'},
 			{ date: '2014/11/14', entry: 'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'}
 		],
-		"Tuesday" : [
-			{ date: '2014/10/31', entry: 'Tuesday entry only one'}
-		],
 		"Thursday" : [
 			{ date: '2014/11/01', entry: 'Thursday entry only one'}
-		]	
+		],
+		"Friday" : [
+			{ date: '2014/11/02', entry: 'Friday entry only one'}
+		],
+		"Saturday" : [
+			{ date: '2014/11/03', entry: 'Saturday entry only one'}
+		],
+		"Sunday" : [
+			{ date: '2014/11/03', entry: 'Saturday entry only one'}
+		]		
 	}
 
 	var questions = {
-		"Wednesday" : "What was something you were nervous to do?",
+		"Monday" : "Where do you live?",
 		"Tuesday" : "What did you eat today?",
-		"Thursday" : "What is one thing that made you happy?"
+		"Wednesday" : "If you could add one more hour to your day, what would you do with it?",
+		"Thursday" : "What is one thing that made you happy?",
+		"Friday" : "How much spare change do you have?",
+		"Saturday" : "Who are the most important people in your life?",
+		"Sunday" : "What was something you were nervous to do?"
 	}
 
 	// $scope.answers = [
