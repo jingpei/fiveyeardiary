3b8f5c7c40e9ed160827067c803b37c9285f66e6
jingpei <jingjpei@gmail.com>
2015-05-21 23:49:20 -0400

    added files for login/registration

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 var diaryApp = angular.module("diaryApp", ["ngRoute", "firebase"]).constant('FIREBASE_URL', 'https://fiveyeardiary.firebaseio.com/');
 
-diaryApp.config(function($routeProvider){
+diaryApp.config(function($routeProvider, $locationProvider){
 	$routeProvider
 		//.when('/', {controller: "MultiDayListController", templateUrl: "app/partials/multi_day_view.html"})
 		.when('/', {controller: "SingleDayListController", templateUrl: "app/partials/single_day_list_view.html"})
 		.when('/:week_day', {controller: "SingleViewListController", templateUrl: "app/partials/single_read_only_list_view.html"})
 		.when('/404', {templateUrl: "app/partials/404.html"})
 		.otherwise( {redirectTo: "/404"} );
+
+	$locationProvider.html5Mode(true);
 });
